d8fa3a64515ac1795f18a37469752a3a56d0837a
kvorobyova <90031119+kvorobyova@users.noreply.github.com>
2021-12-13 00:18:22 +0300

    Add files via upload

--- quiz_code.js
+++ quiz_code.js
@@ -6,41 +6,42 @@ window.onload = function(){
     document.querySelector(".question").innerHTML = quiz[step]['q'];
     let answer = '';
     for (let key in quiz[step]['ans']) {
-      answer += `<li data-v="${key}" class="answer-variant">${quiz[step]['ans'][key]}</li>`;
+      answer += `<li data-v="${key}" class="answer-variant"><div class="select"></div>${quiz[step]['ans'][key]}</li>`;
     }
     document.querySelector(".answer").innerHTML = answer;
   }
   document.onclick = function(event) {
     event.stopPropagation();
     if (event.target.classList.contains('answer-variant') && step < quiz.length) {
-      if (result[event.target.dataset.v] != undefined) {
+      if (result[event.target.dataset.v] !== undefined) {
         result[event.target.dataset.v]++;
       }
       else {
         result[event.target.dataset.v] = 0;
       }
       step++;
-      if (step == quiz.length) {
-        document.querySelector('.question').remove();
+      if (step === quiz.length) {
+        document.querySelector('.question').style.display = 'none';
         document.querySelector('.answer').remove();
+        document.querySelector('.loader').style.display = 'block';
         showResult();
       }
       else {
         showQuestion(step);
       }
-      console.log(result);
     }
     function showResult(){
       let key = Object.keys(result).reduce(function(a,b){
         return result[a] > result[b] ? a:b;
 
       });
-      console.log(key);
-
-      let div = document.createElement('div');
-      div.classList.add('result');
-      div.innerHTML = answers[key]['description'];
-      document.querySelector('main').appendChild(div);
+      document.querySelector('.question').innerText = answers[key]['description'];
+      document.querySelector('button a').href = answers[key].url;
+      setTimeout(() => {
+        document.querySelector('.question').style.display = 'flex';
+        document.querySelector('button').style.display = 'block';
+        document.querySelector('.loader').style.display = 'none';
+      }, 2000);
     }
 
   }
