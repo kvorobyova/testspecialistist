99ddb26fbfb24455fb86e3cb9b9b4079587b190d
kvorobyova <90031119+kvorobyova@users.noreply.github.com>
2021-12-13 10:50:08 +0300

    Add files via upload

--- quiz_code.js
+++ quiz_code.js
@@ -1,49 +1,58 @@
 window.onload = function(){
-  let result = {};
-  let step = 0;
+    let result = {};
+    let step = 0;
 
-  function  showQuestion(questionNumber) {
-    document.querySelector(".question").innerHTML = quiz[step]['q'];
-    let answer = '';
-    for (let key in quiz[step]['ans']) {
-      answer += `<li data-v="${key}" class="answer-variant"><div class="select"></div>${quiz[step]['ans'][key]}</li>`;
+    function  showQuestion(questionNumber) {
+        document.querySelector(".question").innerHTML = quiz[step]['q'];
+        let answer = '';
+        for (let key in quiz[step]['ans']) {
+            answer += `<li data-v="${key}" class="answer-variant"><div class="select"></div>${quiz[step]['ans'][key]}</li>`;
+        }
+        document.querySelector(".answer").innerHTML = answer;
     }
-    document.querySelector(".answer").innerHTML = answer;
-  }
-  document.onclick = function(event) {
-    event.stopPropagation();
-    if (event.target.classList.contains('answer-variant') && step < quiz.length) {
-      if (result[event.target.dataset.v] !== undefined) {
-        result[event.target.dataset.v]++;
-      }
-      else {
-        result[event.target.dataset.v] = 0;
-      }
-      step++;
-      if (step === quiz.length) {
-        document.querySelector('.question').style.display = 'none';
-        document.querySelector('.answer').remove();
-        document.querySelector('.loader').style.display = 'block';
-        showResult();
-      }
-      else {
-        showQuestion(step);
-      }
-    }
-    function showResult(){
-      let key = Object.keys(result).reduce(function(a,b){
-        return result[a] > result[b] ? a:b;
+    document.onclick = function(event) {
+        event.stopPropagation();
+        if (event.target.classList.contains('answer-variant') && step < quiz.length) {
+            if (result[event.target.dataset.v] !== undefined) {
+                result[event.target.dataset.v]++;
+            }
+            else {
+                result[event.target.dataset.v] = 0;
+            }
+            step++;
+            if (step === quiz.length) {
+                document.querySelector('.question').style.display = 'none';
+                document.querySelector('.answer').remove();
+                document.querySelector('.loader').style.display = 'block';
+                showResult();
+            }
+            else {
+                showQuestion(step);
+            }
 
-      });
-      document.querySelector('.question').innerText = answers[key]['description'];
-      document.querySelector('button a').href = answers[key].url;
-      setTimeout(() => {
-        document.querySelector('.question').style.display = 'flex';
-        document.querySelector('button').style.display = 'block';
-        document.querySelector('.loader').style.display = 'none';
-      }, 2000);
-    }
+            document.querySelector('.back').onclick = () => {
+                step--;
+                showQuestion(step);
+                document.querySelector('.back').style.display = step > 0 ? 'block' : 'none';
+            }
 
-  }
-  showQuestion(step);
+            document.querySelector('.back').style.display = step > 0 && step < quiz.length ? 'block' : 'none';
+
+        }
+        function showResult(){
+            let key = Object.keys(result).reduce(function(a,b){
+                return result[a] > result[b] ? a:b;
+            });
+            document.querySelector('.question').innerText = answers[key]['description'];
+            document.querySelector('button a').href = answers[key].url;
+            document.querySelector('.back').style.display = 'none';
+            setTimeout(() => {
+                document.querySelector('.question').style.display = 'flex';
+                document.querySelector('.buttons').style.display = 'flex';
+                document.querySelector('.loader').style.display = 'none';
+            }, 2000);
+        }
+
+    }
+    showQuestion(step);
 }
